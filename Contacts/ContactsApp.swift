1c5d6edc73af689757148905c21abaa1bcada6bc
Dom Montalto <dommontalto@icloud.com>
2025-04-15 14:49:21 +0700

    Initial commit

---
+++ Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by Dominic Montalto on 15/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
